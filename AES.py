8eee8814fd154d47c4642177685e1232df155036
Kamruzzaman Asif <72350695+KamruzzamanAsif@users.noreply.github.com>
2022-08-16 22:19:39 +0600

    matrix formaiton added

--- AES.py
+++ AES.py
@@ -80,6 +80,35 @@ def string_to_hex_converter(string):
 
 
 
+def plainHexValues_to_matrix_converter(hex_value):
+    #########################################################################
+    #       sample input : hex_value 0x2b7e151628aed2a6abf7158809cf4f3c     #
+    #       sample output: in a matrix form according to AES                #
+    #########################################################################
+    matrix = []
+    for i in range(16):
+        # right shifting the values and bitwise and with ff to get the byte
+        # for example if we get 2b7e after right shifting the the and with
+        # ff will give output 7e
+        """ 
+        An example of calculation:
+            2b7e in binary = 0010101101111110
+            ff in binary   = 0000000011111111
+            ----------------------------------
+        bitwise and value  = 0000000001111110 = 0x7e
+        """
+        byte = (hex_value >> (8 * (15 - i))) & 0xFF
+        # this arranges row wise.. we have to arrange column wise
+        if i % 4 == 0:
+            matrix.append([byte])
+        else:
+            matrix[int(i / 4)].append(byte)
+    return matrix
+
+
+
+
+
 def Rot_word(word):
     return word[1:] + word[:1]
 
