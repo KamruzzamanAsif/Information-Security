31f3f5f901a3557e262f51d513d62f196ab7697b
asif_1217 <asif720kamruzzaman@gmail.com>
2022-09-28 09:11:40 +0600

    Exam done!

---
+++ LabFinal/BSSE 1217/fileHandeler.py
@@ -0,0 +1,29 @@
+import AES_CTR
+
+print(">>>>>>>Welcome to AES_CTR-128bit Encryption Decryption<<<<<<<<")
+print("[Note:  1. text is taken form input.txt file\n\t2. cipher is stored in output1.txt file\n\t3. decrypted plain text is stored in output2.txt file]")
+print()
+
+f = open("/home/asif_1217/Information-Security/LabFinal/BSSE 1217/input.txt", "r")
+text = f.read()
+f.close()
+
+print("Enter a 16 byte key as string: ", end=" ")
+key = input()
+
+print("Enter a 16 byte counter as string: ", end=" ")
+counter = input()
+
+cipher = AES_CTR.Encrypt(text, key, counter)
+fout1 = open("/home/asif_1217/Information-Security/LabFinal/BSSE 1217/output1.txt", "w+")
+fout1.write(cipher)
+fout1.close()
+
+print("Encryption Done!")
+
+plainText = AES_CTR.Decrypt(cipher, key, counter)
+fout2 = open("/home/asif_1217/Information-Security/LabFinal/BSSE 1217/output2.txt", "w+")
+fout2.write(plainText)
+fout2.close()
+
+print("Decryption Done!")
